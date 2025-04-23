c20073d3d07a17ebcf84fccf65431c9719638151
MontanaNIcholas <Montana171204@gmail.com>
2025-04-23 16:17:54 +0100

    changes on contact page

--- montana-portfolio/src/routes/contact/+page.svelte
+++ montana-portfolio/src/routes/contact/+page.svelte
@@ -8,40 +8,48 @@
 
 
 <div class="page-bg"></div>
-<div class="contact"><img src="contact-title.png" alt="contact" width="500" height="60" ></div>
 
-<div class="details"><br>
-    <div class="instagram"><img src="instagram.png" alt="insta" width="40" height="40"></div>
-    <b> ADDRESS: </b>
-   
-    <div class="call"><img src="call.png" alt="call" width="40" height="40"></div>
-    <b> PHONE: </b>
-    
-    <div class="mail"><img src="mail.png" alt="mail" width="40" height="40"></div>
-    <b> EMAIL: </b>
 
-    <div class="linkedin"><img src="linkedin.png" alt="linkedin" width="40" height="40"></div>
-    <b> ADDRESS: </b>
-    <br>
-    </div>
+    <div class="details">
+
+        <section class="contact">
+        <div class="pic"><img src="contact-title.png" alt="contact" width="350" height="60"></div>
+          
+            <ul class="contact__icons">
+                <li class="contact__item">
+                    <img src="instagram.png" alt="instagram" />
+                    <span>Instagram</span>
+                  </li>
+              
+                  <li class="contact__item">
+                    <img src="mail.png" alt="mail" />
+                    <span>Mail</span>
+                  </li>
+              
+                  <li class="contact__item">
+                    <img src="call.png" alt="Call" />
+                    <span>Call</span>
+                  </li>
+
+                  <li class="contact__item">
+                    <img src="linkedin.png" alt="linkedin" />
+                    <span>Linkedin</span>
+                  </li>
+            </ul>
+          </section>
+        
+        </div>
    
 <style>
 
-    .contact {
-    width: 60%;
-    position: fixed;
-    top: 15%;
-    margin-left: 20%;
-    transform: translateY(-50%);
-}
-
+   
 .details {
     position: fixed;
     top: 55%;
-    margin-left: 50%;
+    margin-left: 30%;
     transform: translateY(-50%);
-    width: 500px; 
-    height: 350px;
+    width: 550px; 
+    height: 400px;
     background-color: rgb(150, 28, 69);
     border: 5px solid #ffffff;
     border-radius: 4px;
@@ -50,12 +58,42 @@
 
 }
 
-b{  
-    font-size: 20px;
-    top: 50%;
-    margin-left: 50%;
+.contact {
+
+    position: fixed;
+    top: 45%;
+    margin-left: 17%;
+    transform: translateY(-50%);
 }
 
+.contact__icons {
+  list-style: none;
+  padding: 0;
+  margin: 3rem 0 0;            
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;                
+}
+
+.contact__icons img {
+  width: 50px;                 
+
+}
+
+.contact__item {
+  display: flex;
+  align-items: center;      /* vertical alignment of icon & text */
+  gap: 1rem;                /* space between icon and its label   */
+  font: 700 1.1rem 'Press Start 2P', sans-serif; /* pixel font    */
+  color: #ffffff;
+  text-shadow: 0 2px #000;
+}
+
+.contact__item img {
+  width: 60px;              
+
+}
 
 .page-bg{ 
     position: fixed;
