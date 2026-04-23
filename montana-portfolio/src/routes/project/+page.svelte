8559d3e11c39e45a530e5b400657c209ac944daa
Montana Nicholas <148532409+MontanaNicholas@users.noreply.github.com>
2026-04-23 15:33:31 +0100

    Create +page.svelte

---
+++ montana-portfolio/src/routes/project/+page.svelte
@@ -0,0 +1,20 @@
+<h1>Final Year Project</h1>
+
+<p>
+This project is an interactive scrapbook style application developed using the Godot engine.
+It includes multiple minigames such as:
+</p>
+
+<ul>
+  <li>Drink making simulation</li>
+  <li>Birthday candle reaction game</li>
+  <li>Travel scrapbook game</li>
+</ul>
+
+<p>
+The project is currently in development. A playable version will be available soon.
+</p>
+
+<h2>Preview</h2>
+
+<p>Screenshots will be added here.</p>
